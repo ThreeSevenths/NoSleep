f57e4b658991cc488528d34f3cfeda38bdbeef61
Trevor Davis <tdavis@gerrie.com>
2018-01-04 10:00:24 -0500

    Initial Commit of Source.

---
+++ NoSleep/NativeMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NoSleep
+{
+    internal static class NativeMethods
+    {
+        [Flags]
+        internal enum EXECUTION_STATE : uint
+        {
+            ES_CONTINUOUS = 0x80000000,
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_USER_PRESENT = 0x00000004,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+        }
+
+        [DllImport("KERNEL32", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+    }
+}
---
+++ NoSleep/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NoSleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Error: Platform not supported. This app only works with Windows");
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("NoSleep. Your PC will not go to sleep or turn off the display.");
+
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Press any key to restore normal sleep...");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            var result = NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.ES_DISPLAY_REQUIRED | NativeMethods.EXECUTION_STATE.ES_CONTINUOUS);
+
+            Console.Read();
+            
+            Console.WriteLine("Restoring sleep operation");
+
+            result = NativeMethods.SetThreadExecutionState(NativeMethods.EXECUTION_STATE.ES_CONTINUOUS);
+        }
+    }
+}
